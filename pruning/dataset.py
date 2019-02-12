a45a35b09db79650444a52b1ff6e005ef65af0fb
ramitpahwa <ramitpahwa123@gmail.com>
2019-02-12 19:26:44 +0530

    cifar100

--- pruning/dataset.py
+++ pruning/dataset.py
@@ -12,13 +12,19 @@ from PIL import Image
 import glob
 import os
 import data_loader
-
+'''
 # cifar-10 name-class map
 name_class={'airplane':0,'automobile':1,'bird':2,'cat':3,'deer':4,'dog':5,'frog':6,'horse':7,'ship':8,'truck':9}
 name = ['dog','cat','deer','horse']
 name_cifar10_vehicles = ['airplane','automobile','truck']
 name_exp1 = ['dog','cat']
-#cifar-100 name-class map TO-DO
+# TO-DO Random experiment
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='latin1')
+    return dict
 
 def loader(path, batch_size=32, num_workers=4, pin_memory=True):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -40,6 +46,47 @@ def test_loader(path, batch_size=32, num_workers=4, pin_memory=True):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     return data.DataLoader(
         data_loader.CIFARSel(root = path,names = name_cifar10_vehicles, name_class=name_class,train=False,
+                             transform = transforms.Compose([
+                                 transforms.Scale(256),
+                                 transforms.CenterCrop(224),
+                                 transforms.ToTensor(),
+                                 normalize,
+                             ])),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=pin_memory)
+
+'''
+insect_name = ['bee', 'beetle', 'butterfly', 'caterpillar', 'cockroach'] 
+fruit_name = ['apple', 'mushroom', 'orange', 'pear', 'sweet_pepper']
+#cifar-100 name-class map 
+meta = unpickle('../data/cifar-100-python/meta')
+name_class_cifar100 = {}
+for i,name in enumerate(meta['fine_label_names']):
+    name_class_cifar100[name]=i
+# uncomment from below 
+
+def loader(path, batch_size=32, num_workers=4, pin_memory=True):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    return data.DataLoader(
+        data_loader.CIFAR100Sel(root = path,names = insect_name ,name_class=name_class_cifar100,train=True,
+                             transform = transforms.Compose([
+                                 transforms.Scale(256),
+                                 transforms.RandomSizedCrop(224),
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.ToTensor(),
+                                 normalize,
+                             ])),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=pin_memory)
+
+def test_loader(path, batch_size=32, num_workers=4, pin_memory=True):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    return data.DataLoader(
+        data_loader.CIFAR100Sel(root = path,names = insect_name, name_class=name_class_cifar100,train=False,
                              transform = transforms.Compose([
                                  transforms.Scale(256),
                                  transforms.CenterCrop(224),
