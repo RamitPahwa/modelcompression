090121a24c447f0d2df8f74000b14bb4b523b631
ramitpahwa <ramitpahwa123@gmail.com>
2019-02-13 16:08:01 +0530

    test script

--- pruning/test.py
+++ pruning/test.py
@@ -6,9 +6,85 @@ import numpy as np
 import data_loader
 import dataset
 import time 
+
+
 warnings.filterwarnings('ignore')
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
+class ModifiedVGG16Model(torch.nn.Module):
+	def __init__(self):
+		super(ModifiedVGG16Model, self).__init__()
+
+		model = models.vgg16(pretrained=True)
+		self.features = model.features
+
+		for param in self.features.parameters():
+			param.requires_grad = False
+
+		self.classifier = nn.Sequential(
+		    nn.Dropout(),
+		    nn.Linear(25088, 4096),
+		    nn.ReLU(inplace=True),
+		    nn.Dropout(),
+		    nn.Linear(4096, 4096),
+		    nn.ReLU(inplace=True),
+		    nn.Linear(4096, 5))
+
+	def forward(self, x):
+		x = self.features(x)
+		x = x.view(x.size(0), -1)
+		x = self.classifier(x)
+		return x
+
+class ModifiedVGG19Model(torch.nn.Module):
+	def __init__(self):
+		super(ModifiedVGG19Model, self).__init__()
+
+		model = models.vgg19(pretrained=True)
+		self.features = model.features
+
+		for param in self.features.parameters():
+			param.requires_grad = False
+
+		self.classifier = nn.Sequential(
+		    nn.Dropout(),
+		    nn.Linear(25088, 4096),
+		    nn.ReLU(inplace=True),
+		    nn.Dropout(),
+		    nn.Linear(4096, 4096),
+		    nn.ReLU(inplace=True),
+		    nn.Linear(4096, 4))
+
+	def forward(self, x):
+		x = self.features(x)
+		x = x.view(x.size(0), -1)
+		x = self.classifier(x)
+		return x
+
+class ModifiedVGG11Model(torch.nn.Module):
+	def __init__(self):
+		super(ModifiedVGG11Model, self).__init__()
+
+		model = models.vgg11(pretrained=True)
+		self.features = model.features
+
+		for param in self.features.parameters():
+			param.requires_grad = False
+
+		self.classifier = nn.Sequential(
+		    nn.Dropout(),
+		    nn.Linear(25088, 4096),
+		    nn.ReLU(inplace=True),
+		    nn.Dropout(),
+		    nn.Linear(4096, 4096),
+		    nn.ReLU(inplace=True),
+		    nn.Linear(4096, 4))
+
+	def forward(self, x):
+		x = self.features(x)
+		x = x.view(x.size(0), -1)
+		x = self.classifier(x)
+		return x
 
 def numParams(model):
         return sum([len(w.view(-1)) for w in model.parameters()])
@@ -18,8 +94,8 @@ def test(model, test_loader):
     global best_accuracy
     correct = 0
     for idx, (data, target) in enumerate(test_loader):
-        # if cuda:
-        #     data, target = data.cuda(), target.cuda()
+        if cuda:
+            data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
 
         # do the forward pass
@@ -38,17 +114,32 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type = str, default='model')
     parser.add_argument("--testpath", type = str, default='../data')
+    parser.add_argument("--arch", type=str, default="VGG16")
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = get_args()
+    if args.arch == "VGG16": 
+        if torch.cuda.is_available():
+            model = ModifiedVGG16Model().cuda()
+        else:
+            model = ModifiedVGG16Model()
+    elif args.arch == "VGG19":
+        if torch.cuda.is_available():
+            model = ModifiedVGG19Model().cuda()
+        else:
+            model = ModifiedVGG19Model()
+    elif args.arch == "VGG11":
+        if torch.cuda.is_available():
+            model = ModifiedVGG11Model().cuda()
+        else:
+            model = ModifiedVGG11Model()
 
-    # print(torch.cuda.is_available())
-    # if torch.cuda.is_available():
-    #     model = torch.load(args.model).cuda()
-    # else :
-    model = torch.load(args.model)
+    model.load_state_dict(torch.load(args.model))
+    
+    print(torch.cuda.is_available())
+    
     test_path = args.testpath 
     test_loader = dataset.test_loader(test_path)
     start_time = time.time()
