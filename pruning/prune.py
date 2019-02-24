0f721e37c587ed80a58d158c3d34157741fa2586
ramitpahwa <ramitpahwa123@gmail.com>
2019-02-24 08:08:33 +0530

    done

--- pruning/prune.py
+++ pruning/prune.py
@@ -11,6 +11,7 @@ def replace_layers(model, i, indexes, layers):
 	return model[i]
 
 def prune_vgg16_conv_layer(model, layer_index, filter_index):
+	print(layer_index)
 	_, conv = list(model.features._modules.items())[layer_index]
 	next_conv = None
 	offset = 1
